b48c547aba64bdfab3c87eca085c0b633ddc20de
Arleycht <47849356+Arleycht@users.noreply.github.com>
2025-09-24 19:24:12 -0400

    Refactor code

--- ytchat.py
+++ ytchat.py
@@ -1,15 +1,105 @@
+import json
 import re
 import requests
-from typing import Dict, Any
+from pathlib import Path
+from typing import Any, Dict
 
 
 class ChatOptions:
     def __init__(self):
-        self.live_id: str = ""
-        self.is_replay: bool = False
         self.api_key: str = ""
         self.client_version: str = ""
         self.continuation: str = ""
+        self.is_replay: bool = False
+        self.live_id: str = ""
+
+
+class ChatAction:
+    def __init__(self):
+        """
+        "authorBadges",
+        "authorExternalChannelId",
+        "authorName",
+        "authorPhoto",
+        "id",
+        "message",
+        "timestampUsec",
+        """
+
+        self.action_type = "addChatItemAction"
+        self.item_type = "liveChatTextMessageRenderer"
+
+        self.id = ""
+        self.timestamp_usec = 0
+
+        self.author_external_channel_id = ""
+        self.author_name = ""
+        self.author_badges = ""
+        self.author_photo = ""
+
+        self.message = ""
+
+
+class Chat:
+    def __init__(self, stream_url: str):
+        response = requests.get(stream_url)
+        response.raise_for_status()
+
+        data = response.text
+        options = get_options_from_live_page(data)
+
+        self.history = []
+        self.options = options
+        self.dirty = True
+
+    def _raw_fetch(self) -> list[Any]:
+        """
+        Fetch without history modification
+        """
+
+        new_history, continuation = fetch_chat(self.options)
+        self.options.continuation = continuation
+
+        return new_history
+
+    def fetch(self) -> list[Any]:
+        new_history = self._raw_fetch()
+        self.history += new_history
+
+        return new_history
+
+    def load_history(self, file: str | Path):
+        """
+        Load previous chat history from a file and updates history with new chats.
+
+        Returns if the loaded history and current history intersected and the amount of new chats added.
+        """
+
+        with open(file, "r") as f:
+            self.history = json.load(f)
+
+        current_history = self._raw_fetch()
+
+        latest_timestamp_usec = get_timestamp_usec(self.history[-1])
+        new_history = [
+            x for x in current_history if get_timestamp_usec(x) > latest_timestamp_usec
+        ]
+
+        self.history += new_history
+
+        return len(current_history) > len(new_history), len(new_history)
+
+    def write_history(self, file: str):
+        with open(file, "w") as f:
+            json.dump(self.history, f, indent=4)
+
+
+def get_timestamp_usec(raw_entry):
+    return int(
+        raw_entry["addChatItemAction"]["item"]["liveChatTextMessageRenderer"][
+            "timestampUsec"
+        ]
+    )
 
 
 def get_options_from_live_page(data: str) -> ChatOptions:
@@ -111,7 +201,38 @@ def strip_actions(actions_dict: Dict[str, Any]):
     return result
 
 
-def fetch_chat(options: ChatOptions):
+def convert(actions_obj) -> list[ChatAction]:
+    actions = []
+
+    for action_name, item in actions_obj.items():
+        if action_name == "addChatItemAction":
+            renderer = item.get("liveChatTextMessageRenderer", {})
+
+            if renderer:
+                chat_action = ChatAction()
+
+                chat_action.action_type = "addChatItemAction"
+                chat_action.item_type = "liveChatTextMessageRenderer"
+
+                chat_action.id = renderer["id"]
+                chat_action.timestamp_usec = get_timestamp_usec(actions_obj)
+
+                chat_action.author_external_channel_id = renderer[
+                    "authorExternalChannelId"
+                ]
+
+                chat_action.author_name = renderer["authorName"]
+                chat_action.author_badges = renderer["authorBadges"]
+                chat_action.author_photo = renderer["authorPhoto"]
+
+                chat_action.message = renderer["message"]["simpleText"]
+
+                actions.append(chat_action)
+
+    return actions
+
+
+def fetch_raw(options: ChatOptions):
     url = f"https://www.youtube.com/youtubei/v1/live_chat/get_live_chat?key={options.api_key}"
 
     response = requests.post(
@@ -126,36 +247,52 @@ def fetch_chat(options: ChatOptions):
             "continuation": options.continuation,
         },
     )
+    response.raise_for_status()
 
-    data = response.json()
-
+    response = response.json()
     raw_history = []
 
     if (
-        data.get("continuationContents", {})
+        response.get("continuationContents", {})
         .get("liveChatContinuation", {})
         .get("actions")
     ):
         raw_history = strip_tracking(
-            response.json()["continuationContents"]["liveChatContinuation"]["actions"]
+            response["continuationContents"]["liveChatContinuation"]["actions"]
+        )
+
+    return response, raw_history
+
+
+def fetch_chat(options: ChatOptions) -> tuple[list[Any], str]:
+    response, raw_history = fetch_raw(options)
+
+    if (
+        response.get("continuationContents", {})
+        .get("liveChatContinuation", {})
+        .get("actions")
+    ):
+        raw_history = strip_tracking(
+            response["continuationContents"]["liveChatContinuation"]["actions"]
         )
 
     continuation_data = (
-        response.json()
-        .get("continuationContents", {})
+        response.get("continuationContents", {})
         .get("liveChatContinuation", {})
         .get("continuations", [{}])[0]
     )
 
-    continuation = ""
+    new_continuation = ""
 
     if continuation_data.get("invalidationContinuationData"):
-        continuation = continuation_data["invalidationContinuationData"]["continuation"]
+        new_continuation = continuation_data["invalidationContinuationData"][
+            "continuation"
+        ]
     elif continuation_data.get("timedContinuationData"):
-        continuation = continuation_data["timedContinuationData"]["continuation"]
+        new_continuation = continuation_data["timedContinuationData"]["continuation"]
 
     # with open(f"{options.live_id}_raw.json", "w") as f:
-    #     json.dump(response.json(), f, indent=4)
+    #     json.dump(response, f, indent=4)
 
     stripped_history = []
 
@@ -172,4 +309,4 @@ def fetch_chat(options: ChatOptions):
     # with open(f"{options.live_id}.json", "w") as f:
     #     json.dump(stripped_history, f, indent=4)
 
-    return stripped_history, continuation
+    return stripped_history, new_continuation
--- ytrecordchat.py
+++ ytrecordchat.py
@@ -9,6 +9,43 @@ from pathlib import Path
 import ytchat as ytc
 
 
+def live_record(chat: ytc.Chat, output_path: Path):
+    print(f"[{datetime.now()}] Started live chat recording")
+
+    last_new_chat_time = time.time()
+    is_stale = False
+    STALE_TIME = 120
+
+    DT = 1
+
+    while True:
+        try:
+            new_history = chat.fetch()
+
+            if len(new_history) > 0:
+                if is_stale:
+                    print(f"{len(new_history)} new chat messages after going stale")
+
+                last_new_chat_time = time.time()
+                is_stale = False
+            elif time.time() - last_new_chat_time > STALE_TIME and not is_stale:
+                is_stale = True
+
+                # Warn user that the chat is going stale, which could be a slow chat or
+                # a bug within this program.
+                print(f"No new chats have been seen for over {STALE_TIME} seconds")
+
+            chat.write_history(output_path)
+
+            time.sleep(DT)
+        except Exception as e:
+            print(f"Encountered exception: {e}")
+            print(f"[{datetime.now()}] Breaking out of loop after exception")
+            break
+
+    print(f"Stopped live chat recording at {datetime.now()}")
+
+
 def main():
     parser = argparse.ArgumentParser(description="YouTube Live Chat Recorder")
     parser.add_argument("url", help="YouTube live stream URL")
@@ -18,83 +55,32 @@ def main():
 
     args = parser.parse_args()
 
-    response = requests.get(args.url)
-    response.raise_for_status()
+    chat = ytc.Chat(args.url)
 
-    data = response.text
-    options = ytc.get_options_from_live_page(data)
+    output_path = args.output if args.output else f"{chat.options.live_id}.json"
 
-    output_file = args.output if args.output else f"{options.live_id}.json"
+    if Path(output_path).exists():
+        print(f"Loading existing history from {output_path}")
 
-    history = []
-    latest_time_usec = 0
+        does_history_intersect, new_chat_count = chat.load_history(output_path)
+        latest_timestamp_usec = ytc.get_timestamp_usec(chat.history[-1])
 
-    if Path(output_file).exists():
-        print(f"Loading existing history from {output_file}")
+        print(
+            f"Found previous data that ended at [{datetime.fromtimestamp(latest_timestamp_usec / 1_000_000)}]"
+        )
 
-        with open(output_file, "r") as f:
-            history = json.load(f)
-
-        for entry in history:
-            timestamp_usec = int(
-                entry["addChatItemAction"]["item"]["liveChatTextMessageRenderer"][
-                    "timestampUsec"
-                ]
+        if does_history_intersect:
+            print(f"[{datetime.now()}] Added {new_chat_count} new chats")
+        else:
+            print(
+                "\033[93mWARNING: All chat history from the server is newer, meaning there might be missed messages in between recovered data and current data!\033[0m"
             )
 
-            latest_time_usec = max(timestamp_usec, latest_time_usec)
-
-        recovered_timestamp = datetime.fromtimestamp(latest_time_usec / 1_000_000)
-        print(f"Found previous data that ended at [{recovered_timestamp}]")
-
-    print(f"[{datetime.now()}] Started")
-
-    while True:
-        try:
-            new_history, new_continuation = ytc.fetch_chat(options)
-            options.continuation = new_continuation
-
-            if latest_time_usec > 0:
-                # Chat position recovery
-
-                appended_count = 0
-
-                for entry in new_history:
-                    timestamp_usec = int(
-                        entry["addChatItemAction"]["item"][
-                            "liveChatTextMessageRenderer"
-                        ]["timestampUsec"]
-                    )
-
-                    if timestamp_usec > latest_time_usec:
-                        history.append(entry)
-                        appended_count += 1
-
-                if appended_count == len(new_history):
-                    print(
-                        "\033[93mWARNING: All chat history from the server is newer, meaning there might be missed messages in between recovered data and current data!\033[0m"
-                    )
-                else:
-                    print(
-                        f"[{datetime.now()}] Skipped {len(new_history) - appended_count} existing chats, added {appended_count} new chats"
-                    )
-
-                latest_time_usec = 0
-            elif len(new_history) > 0:
-                history += new_history
-
-                # print(f"[{datetime.now()}] Appended {len(new_history)} new messages")
-
-            with open(output_file, "w") as f:
-                json.dump(history, f, indent=4)
-
-            time.sleep(1)
-        except Exception as e:
-            print(e)
-            print(f"[{datetime.now()}] Breaking out of loop after exception")
-            break
-
-    print(f"Stopped at time {datetime.now()}")
+    if chat.options.is_replay:
+        print("Stream is offline")
+        raise NotImplemented("Replay and live chat history merging")
+    else:
+        live_record(chat, output_path)
 
 
 if __name__ == "__main__":
